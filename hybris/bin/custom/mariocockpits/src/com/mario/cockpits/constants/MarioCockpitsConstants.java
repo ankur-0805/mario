21e5830fa2ec95e63b79a849e6dda81df17a8f72
Ankur Agarwal <aagarwal26@sapient.com>
2017-11-12 10:51:36 +0530

    master - Addign mario cockpit extension.

---
+++ hybris/bin/custom/mariocockpits/src/com/mario/cockpits/constants/MarioCockpitsConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.mario.cockpits.constants;
+
+/**
+ * Global class for all MarioCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class MarioCockpitsConstants extends GeneratedMarioCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "mariocockpits";
+
+	public static final String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private MarioCockpitsConstants()
+	{
+		// empty
+	}
+}
